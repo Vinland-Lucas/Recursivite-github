7df09b27c435e9d924a33d76fda93bd766171043
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2023-08-04 16:45:22 +0200

    UPDT PGCD by commenting the code

--- src/fr/campus/recursivite/PGCD/PGCD.java
+++ src/fr/campus/recursivite/PGCD/PGCD.java
@@ -9,7 +9,7 @@ public class PGCD {
         if (b == 0) {
             return a;
         } else {
-            return PGCD_recursive(b, a % b);
+            return PGCD_recursive(b, a % b); // cf.énoncé ici "PGCD_recursive" = pgcd(a,b) et "(b, a % b)" = pgcd(b, r) --> BIEN RETENIR CA C'EST IMPORTANT POUR REUSSIR LES EXOS
         }
     }
 }
